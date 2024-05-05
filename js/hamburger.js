9aca295a341aaa6217cb2b1b2b8d6290cd402e96
RGalindo2004 <151685550+RGalindo2004@users.noreply.github.com>
2024-05-04 22:52:43 -0600

    hamburger
    hamburger

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,11 +1,19 @@
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
+const overlay = document.querySelector("#overlay");
 
 abrir.addEventListener("click", () => {
     nav.classList.add("visible");
-})
+    document.body.classList.add("abrir-menu");
+});
 
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
-})
+    document.body.classList.remove("abrir-menu");
+});
+
+overlay.addEventListener("click", () => {
+    nav.classList.remove("visible");
+    document.body.classList.remove("abrir-menu");
+});
